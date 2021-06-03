addacd3b444831f36b2ff82984a3a09d0bbfcf7b
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-06-03 14:10:09 -0400

    Removed routes that are no longer necessary, since we're thinking with modals.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -30,21 +30,9 @@ function App() {
             <NavBar/>
             <div className="content-wrapper">
                 <Switch>
-                    <Route path="/login" exact={true}>
-                        <LoginForm/>
-                    </Route>
-                    <Route path="/sign-up" exact={true}>
-                        <SignUpForm/>
-                    </Route>
-                    <ProtectedRoute path="/users" exact={true}>
-                        <UsersList/>
-                    </ProtectedRoute>
-                    <ProtectedRoute path="/users/:userId" exact={true}>
-                        <User/>
-                    </ProtectedRoute>
-                    <ProtectedRoute path="/" exact={true}>
+                    <Route path="/" exact={true}>
                         <h1>My Home Page</h1>
-                    </ProtectedRoute>
+                    </Route>
                 </Switch>
             </div>
         </BrowserRouter>
