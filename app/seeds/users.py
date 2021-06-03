6740a9739705bb99e34d4899358df3791788db87
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-06-03 18:18:20 -0400

    Updated user seed to create 50 users instead of just demo.

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,20 +1,41 @@
-from werkzeug.security import generate_password_hash
 from app.models import db, User
+from faker import Faker
+from random import randint
 
-# Adds a demo user, you can add other users here if you want
+fake = Faker()
+
+
 def seed_users():
+    demo = User(
+        username="Demo",
+        email="demo@aa.io",
+        password="password"
+    )
 
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
+    users = []
+    used_names = set()
+    used_emails = set()
+    for i in range(49):
+        username = fake.user_name()
+        while username in used_names or len(username) > 16:
+            username = fake.user_name()
+        email = fake.email()
+        while email in used_emails:
+            email = fake.email()
+        users.append(
+            User(
+                username=username,
+                email=email,
+                password="password",
+                title=" ".join(fake.words(randint(1, 4)))
+            )
+        )
 
     db.session.add(demo)
-
+    db.session.add_all(users)
     db.session.commit()
 
-# Uses a raw SQL query to TRUNCATE the users table.
-# SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and resets
-# the auto incrementing primary key
+
 def undo_users():
-    db.session.execute('TRUNCATE users;')
+    db.session.execute("TRUNCATE users CASCADE;")
     db.session.commit()
