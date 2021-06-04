b29beaae7527ff3bbb5b8b4cfb7b0378bf93d40c
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-06-04 11:34:57 -0400

    Random change to styling of dropdown menu.

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,27 +1,9 @@
 import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useHistory} from "react-router-dom";
-import Modal from "react-modal";
 import "./NavBar.css";
 import {logout} from "../../store/session";
 import LogRegModal from "./LogRegModal";
-
-const modalStyles = {
-    content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-        backgroundColor: "#272729",
-        border: "none",
-        padding: "0"
-    },
-    overlay: {
-        backgroundColor: "rgba(0, 0, 0, 0.75)"
-    }
-};
 
 const NavBar = () => {
     const user = useSelector(state => state.session.user);
