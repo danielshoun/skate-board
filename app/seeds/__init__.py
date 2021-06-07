50c70bf37394a825a50ef833544f11d94af8c70e
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-06-07 11:26:07 -0400

    Created seeder file for threads. Still need to implement a way to fake dates for seeded threads/posts.

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .boards import seed_boards, undo_boards
+from .threads import seed_threads, undo_threads
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_boards()
+    seed_threads()
     # Add other seed functions here
 
 
@@ -20,4 +22,5 @@ def seed():
 def undo():
     undo_users()
     undo_boards()
+    undo_threads()
     # Add other undo functions here
---
+++ app/seeds/threads.py
@@ -0,0 +1,33 @@
+from app.models import db, Board, Thread, Post
+from faker import Faker
+from random import randint, choice
+
+fake = Faker()
+
+
+# TODO: Add in fake dates to first posts to allow for more realistic looking chronology.
+def seed_threads():
+    for i in range(1, 11):
+        board = Board.query.get(i)
+        for _ in range(randint(10, 61)):
+            thread = Thread(
+                owner_id=choice(board.members).id,
+                board_id=board.id,
+                title=" ".join(fake.words(randint(1, 5))),
+                pinned=True if randint(1, 10) > 9 else False,
+                locked=True if randint(1, 10) > 9 else False
+            )
+            db.session.add(thread)
+            db.session.flush()
+            post = Post(
+                owner_id=thread.owner_id,
+                thread_id=thread.id,
+                body=fake.paragraph() if randint(1, 10) > 5 else fake.sentence()
+            )
+            db.session.add(post)
+    db.session.commit()
+
+
+def undo_threads():
+    db.session.execute("TRUNCATE threads CASCADE;")
+    db.session.commit()
