9969453e15edd03f2928721c1f78213b679f5dc2
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-06-10 11:55:37 -0400

    Added AWS upload handling file and a smilie route that will hopefully one day use it.

--- app/api/smilie_routes.py
+++ app/api/smilie_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from flask_login import login_required, current_user
 from app.models import Board, db, Thread, Post, Smilie
 from sqlalchemy import or_
 
@@ -9,3 +10,14 @@ smilie_routes = Blueprint("smilies", __name__)
 def get_smilies(board_id):
     smilies = Smilie.query.filter(or_(Smilie.board_id == board_id, Smilie.board_id == None)).all()
     return jsonify([smilie.to_dict() for smilie in smilies])
+
+
+@smilie_routes.route("/")
+@login_required
+def create_smilie():
+    board = Board.query.get(request.json["board_id"])
+    if not board:
+        return {"errors": f"No post exists with ID: {request.json['board_id']}"}, 400
+    if not (board.owner_id == current_user.id):
+        return {"errors": "You must be the owner of a board to add smilies."}, 400
+    print(request.files)
